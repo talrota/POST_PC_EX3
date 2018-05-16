48d7cb3b08722233cf29a36106f42dab5968b2f7
talrota <tal.rota@gmail.com>
2018-05-16 12:09:31 +0300

    v1

--- app/src/androidTest/java/com/example/talrota/post_pc_ex3/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/talrota/post_pc_ex3/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.talrota.post_pc_ex3", appContext.getPackageName());
+        assertEquals("al2036.multithreads", appContext.getPackageName());
     }
 }
